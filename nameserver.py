c1aae5a2d74b9c7b142d29b8d6c88bd7846c9f50
tstromberg <tstromberg@google.com>
2010-05-17 19:49:59 +0000

    Update country page

--- nameserver.py
+++ nameserver.py
@@ -18,6 +18,8 @@ import cgi
 import datetime
 import operator
 import os
+import logging
+from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
@@ -48,7 +50,10 @@ class LookupHandler(webapp.RequestHandler):
     }
     path = os.path.join(os.path.dirname(__file__), 'templates', 'nameserver.html')
     self.response.out.write(template.render(path, template_values))
-    
+
+class DummyNameserver(object):
+  name = '(Fastest Local Nameserver)'
+
 class CountryHandler(webapp.RequestHandler):
   """Handler for /ns/### requests."""
   
@@ -56,23 +61,47 @@ class CountryHandler(webapp.RequestHandler):
   def get(self, country_code):
     ns_count = {}
     avg_latencies = {}
-    ns_data = {}
+    ns_data = {
+      '__local__': {
+        'name': '(Fastest local nameserver)',
+        'ip': '__local__',
+        'hostname': '__fastest.local__',
+        'ns': DummyNameserver(),
+        'count': 0,
+        'is_global': True,
+        'overall_position': -1,
+        'positions': [],
+        'averages': [],
+        'results': []
+      }
+    }
     
     country = None
     total = 0
     last_timestamp = None
-    
-    for sub in models.Submission.all().filter("country_code = ", country_code).filter("listed = ", True).order('timestamp'):
+    submissions = self.get_cached_submissions(country_code)
+    for sub in submissions:
       if not country:
         country = sub.country
       if not last_timestamp:
         last_timestamp = sub.timestamp
       total += 1
-      
+
+      fastest_local = None
       for ns_sub in sub.nameservers:
         ip = "%s" % ns_sub.nameserver.ip
         if ip not in ns_data:
-          ns_data[ip] = {'name': ns_sub.nameserver.name, 'ip': ip, 'hostname': ns_sub.nameserver.hostname, 'ns': ns_sub.nameserver }
+          ns_data[ip] = {
+            'name': ns_sub.nameserver.name,
+            'ip': ip,
+            'hostname': ns_sub.nameserver.hostname,
+            'ns': ns_sub.nameserver,
+            'is_global': ns_sub.nameserver.is_global,
+            'overall_position': -1
+        }
+        if not ns_sub.nameserver.is_global and (not fastest_local or ns_sub.overall_average < fastest_local.overall_average):
+          fastest_local = ns_sub
+
         ns_data[ip]['count'] = ns_data[ip].setdefault('count', 0) + 1
         if ns_sub.position < 15:
           ns_data[ip].setdefault('positions', []).append(ns_sub.position)
@@ -80,6 +109,14 @@ class CountryHandler(webapp.RequestHandler):
 
         for run in ns_sub.results:
           ns_data[ip].setdefault('results', []).extend(run.durations)
+      
+      if fastest_local:
+        ns_sub = fastest_local
+        ns_data['__local__']['count'] += 1
+        ns_data['__local__'].setdefault('positions', []).append(ns_sub.position)
+        ns_data['__local__'].setdefault('averages', []).append(ns_sub.overall_average)
+        for run in ns_sub.results:
+          ns_data['__local__'].setdefault('results', []).extend(run.durations)
       
       if sub.primary_nameserver:
         if sub.primary_nameserver.name:
@@ -90,28 +127,34 @@ class CountryHandler(webapp.RequestHandler):
       ns_count[ns_name] = ns_count.setdefault(ns_name, 0) + 1
 
     runs_data = []
-    ns_popular_list = sorted(ns_data, key=operator.attrgetter('count'))
-    ns_popular_list.reverse()
-    for ns in ns_popular_list:
-      if len(runs_data) < 10:
-        if 'results' in ns_data[ns]:
-          runs_data.append((ns_data[ns]['ns'], ns_data[ns]['results']))
-      if 'averages' in ns_data[ns]:
-        ns_data[ns]['overall_average'] = CalculateListAverage(ns_data[ns]['averages'])
+    runs_data_global = []
+    ns_popular_list = sorted(ns_data.values(), key=lambda x:(x['count']), reverse=True)
+    for row in ns_popular_list:
+      if 'results' in row:
+        if row['ip'] != '__local__' and len(runs_data) < 10:
+          runs_data.append((row['ns'], row['results']))
+        if row['is_global']:
+          runs_data_global.append((row['ns'], row['results']))
+      if 'averages' in row:
+        row['overall_average'] = CalculateListAverage(row['averages'])
       else:
-        ns_data[ns]['overall_average'] = -1
-      if 'positions' in ns_data[ns]:
-        ns_data[ns]['overall_position'] = CalculateListAverage(ns_data[ns]['positions'])
+        row['overall_average'] = -1
+      if 'positions' in row:
+        row['overall_position'] = CalculateListAverage(row['positions'])
       else:
-        ns_data[ns]['overall_position'] = -1
-        
+        row['overall_position'] = -1
+  
     template_values = {
       'country_code': country_code,
       'count': total,
       'popular_nameservers': ns_count.items(),
       'nsdata': ns_data.values(),
+      'nsdata_raw': ns_data,
       'country': country,
-      'distribution_url_200': charts.DistributionLineGraph(runs_data, scale=350, sort_by=self._SortDistribution),
+      'submissions': submissions,
+      'recent_submissions': submissions[0:15],
+      'distribution_url': charts.DistributionLineGraph(runs_data, scale=350, sort_by=self._SortDistribution),
+      'distribution_url_global': charts.DistributionLineGraph(runs_data_global, scale=350, sort_by=self._SortDistribution),
       'last_update': last_timestamp
     }
     path = os.path.join(os.path.dirname(__file__), 'templates', 'country.html')
@@ -120,4 +163,20 @@ class CountryHandler(webapp.RequestHandler):
   def _SortDistribution(self, a, b):
     """Sort distribution graph by name (for now)."""
     return cmp(a[0].name, b[0].name)
+    
+  def get_cached_submissions(self, country_code):
+    key = "submissions-%s" % country_code
+    submissions = memcache.get(key)
+    if submissions is not None:
+      return submissions
+
+    query = models.Submission.all()
+    query.filter("country_code =", country_code)
+    query.filter('listed =', True)
+    query.order('-timestamp')
+    submissions = query.fetch(250)
+    if not memcache.add(key, submissions, 7200):
+      logging.error("Memcache set failed.")
+    return submissions
+      
 
